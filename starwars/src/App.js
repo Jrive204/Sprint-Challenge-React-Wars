4018b91dba81bdf6126341f981027d68ad2cf4b2
Jose <Jrivera6869@yahoo.com>
2019-12-14 01:34:16 -0500

    added second api and images for fun

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import { Card, Button, CardTitle, CardText, Form } from "reactstrap";
+import { Card, Button, CardTitle, CardText, Form, CardImg } from "reactstrap";
 import Formsearch from "./components/Formsearch";
 import Page from "./components/Page";
 
@@ -11,6 +11,17 @@ const App = () => {
   const [search, setSearch] = useState(``);
   const [data, setData] = useState(``);
   const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
+
+  let axOne = `https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json`;
+  let axTwo = `https://swapi.co/api/people/?page=${page}&format=json`;
+
+  const requestOne = axios.get(axOne);
+  const requestTwo = axios.get(axTwo);
+
+  const chgimg = () => {
+    setCount(count + 10);
+  };
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
@@ -18,43 +29,69 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get(` https://swapi.co/api/people/?page=${page}&format=json`)
-      .then(response => {
-        let people = response.data.results;
-        let char = people.map(e => {
-          return (
-            <div className='charCarddiv'>
-              <Card
-                className='cards'
-                body
-                inverse
-                style={{
-                  backgroundColor: "#333",
-                  borderColor: "#rgba(51, 51, 51, 0.9)"
-                }}>
-                <ul style={{ listStyle: `none`, paddingRight: `40px` }}>
-                  <li>
-                    <CardTitle>
-                      <h4 id={e.name} key={e.name}>
-                        {e.name}
-                      </h4>
-                    </CardTitle>
-                  </li>
-                  <li>Films: {e.films.length}</li>
-                  <li>
-                    Birth Year: {e.birth_year} &nbsp; Gender: {e.gender}
-                  </li>
-                  <Button className='votebutton' color='danger'>
-                    Vote!
-                  </Button>
-                </ul>
-              </Card>
-            </div>
-          );
-        });
-        setData(char);
-        console.log(page);
-      })
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread(
+          (...response) => {
+            const requestOne = response[0];
+            const requestTwo = response[1];
+
+            let imagechar = requestOne.data;
+            let people = requestTwo.data.results;
+
+            console.log(`firstlist`, imagechar);
+            console.log(`secondlist`, people);
+
+            // console.log(`people`, people);
+            let char = people.map(
+              (e, i) => {
+                return (
+                  <div className='charCarddiv'>
+                    <Card
+                      className='cards'
+                      body
+                      inverse
+                      style={{
+                        backgroundColor: "#333",
+                        borderColor: "#rgba(51, 51, 51, 0.9)"
+                      }}>
+                      <CardImg
+                        src={imagechar[i + count].image}
+                        width='100%'
+                        alt='Card image cap'
+                        style={{ paddingBottom: `4%` }}
+                      />
+                      {console.log(
+                        `imagesbitches`,
+                        imagechar[i].image,
+                        people[i]
+                      )}
+                      <ul style={{ listStyle: `none`, paddingRight: `40px` }}>
+                        <li>
+                          <CardTitle>
+                            <h4 id={e.name} key={e.name}>
+                              {e.name}
+                            </h4>
+                          </CardTitle>
+                        </li>
+                        <li>Films: {e.films.length}</li>
+                        <li>
+                          Birth Year: {e.birth_year} &nbsp; Gender: {e.gender}
+                        </li>
+                        <Button className='votebutton' color='danger'>
+                          Vote!
+                        </Button>
+                      </ul>
+                    </Card>
+                  </div>
+                ); // close return
+              } //close map
+            ); //close e
+            setData(char);
+            console.log(`page`, page);
+          } // close response
+        )
+      ) //close then
       .catch(err => {
         err.msg = `Sorry Reload`;
       });
@@ -79,9 +116,12 @@ const App = () => {
         handlechange={handlechange}></Formsearch>
       <div>{data}</div>
       <Page
-        pageprev={e => (page == 1 ? null : setPage(page - 1))}
+        count={count}
+        pageprev={e =>
+          page == 1 ? null : setPage(page - 1) - setCount(count - 10)
+        }
         page={page}
-        pagechangeradd={e => setPage(page + 1)}></Page>
+        pagechangeradd={e => setCount(count + 10) + setPage(page + 1)}></Page>
     </div>
   );
 };
